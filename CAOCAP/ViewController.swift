694ff7ffb50c2d8cc5b2cf0e16d33c817584e847
Azzam R Alrashed <azzam.rar@gmail.com>
2023-06-04 07:19:35 +0300

    worked on the add node button, and the mechanism of sponging the node in the right place on the mindmap canvas

--- CAOCAP/ViewController.swift
+++ CAOCAP/ViewController.swift
@@ -10,56 +10,11 @@ import WebKit
 
 class ViewController: UIViewController {
     
+    var nodeTree = [UIView]()
     @IBOutlet weak var webview: WKWebView!
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        scrollView.setContentOffset(CGPoint(x: 100, y: 100), animated: false)
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //Setup scrollview
-        view.insertSubview(scrollView, at: 0)
-        scrollView.addSubview(canvas)
-        let firstNode = AddNode()
-        let secondNode = AddNode(color: .systemGreen)
-        let thirdNode = AddNode(color: .systemPink)
-        canvas.addSubview(firstNode)
-        canvas.addSubview(secondNode)
-        canvas.addSubview(thirdNode)
-        
-        NSLayoutConstraint.activate([
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            
-            canvas.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            canvas.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            canvas.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            canvas.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            canvas.heightAnchor.constraint(equalToConstant: view.frame.height + 200),
-            canvas.widthAnchor.constraint(equalToConstant: view.frame.width + 200 ),
-            
-            firstNode.centerXAnchor.constraint(equalTo: canvas.centerXAnchor),
-            firstNode.centerYAnchor.constraint(equalTo: canvas.centerYAnchor),
-            firstNode.heightAnchor.constraint(equalToConstant: 100),
-            firstNode.widthAnchor.constraint(equalToConstant: 200),
-            
-            secondNode.centerXAnchor.constraint(equalTo: canvas.centerXAnchor, constant: 250),
-            secondNode.centerYAnchor.constraint(equalTo: canvas.centerYAnchor, constant: 100),
-            secondNode.heightAnchor.constraint(equalToConstant: 100),
-            secondNode.widthAnchor.constraint(equalToConstant: 200),
-            
-            thirdNode.centerXAnchor.constraint(equalTo: canvas.centerXAnchor, constant: 250),
-            thirdNode.centerYAnchor.constraint(equalTo: canvas.centerYAnchor, constant: -100),
-            thirdNode.heightAnchor.constraint(equalToConstant: 100),
-            thirdNode.widthAnchor.constraint(equalToConstant: 200)
-        ])
-        
-        
-        //Load HTML
-        webview.loadHTMLString("<h1>Hello CAOCAP</h1>", baseURL: nil)
-    }
+    
+    var canvasHeightConstraint = NSLayoutConstraint()
+    var canvasWidthConstraint = NSLayoutConstraint()
     
     let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -74,6 +29,39 @@ class ViewController: UIViewController {
         view.backgroundColor = .secondarySystemFill
         return view
     }()
+
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.setContentOffset(CGPoint(x: 100, y: 100), animated: false)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Setup scrollview
+        view.insertSubview(scrollView, at: 0)
+        scrollView.addSubview(canvas)
+        
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            canvas.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            canvas.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            canvas.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            canvas.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+        ])
+        
+        canvasHeightConstraint = canvas.heightAnchor.constraint(equalToConstant: view.frame.height + 200)
+        canvasWidthConstraint = canvas.widthAnchor.constraint(equalToConstant: view.frame.width + 200 )
+        canvasHeightConstraint.isActive = true
+        canvasWidthConstraint.isActive = true
+        
+        
+        //Load HTML
+        webview.loadHTMLString("<h1>Hello CAOCAP</h1>", baseURL: nil)
+    }
     
     func AddNode(color: UIColor = .systemBlue) -> UIView {
         let view = UIView()
@@ -83,6 +71,31 @@ class ViewController: UIViewController {
         return view
     }
     
-    
+    @IBAction func didPressAdd(_ sender: Any) {
+        let newNode = AddNode()
+        nodeTree.append(newNode)
+        canvas.addSubview(newNode)
+        print(nodeTree.count)
+        
+        if nodeTree.count == 1 {
+            NSLayoutConstraint.activate([
+                newNode.leadingAnchor.constraint(equalTo: canvas.leadingAnchor, constant: canvasWidthConstraint.constant / 2),
+                newNode.centerYAnchor.constraint(equalTo: canvas.centerYAnchor),
+                newNode.heightAnchor.constraint(equalToConstant: 60),
+                newNode.widthAnchor.constraint(equalToConstant: 150),
+            ])
+        } else {
+            canvasHeightConstraint.constant += 90
+            canvasWidthConstraint.constant += 180
+            NSLayoutConstraint.activate([
+                newNode.leadingAnchor.constraint(equalTo: nodeTree[nodeTree.count - 2].trailingAnchor, constant: 30),
+                newNode.bottomAnchor.constraint(equalTo: nodeTree[nodeTree.count - 2].topAnchor, constant: 30),
+                newNode.heightAnchor.constraint(equalToConstant: 60),
+                newNode.widthAnchor.constraint(equalToConstant: 150),
+            ])
+        }
+        
+        
+    }
 }
 
