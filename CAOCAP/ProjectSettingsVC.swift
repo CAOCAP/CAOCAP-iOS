536625e31a11ea00f6d4dd05982b36bbbbc64196
Azzam R Alrashed <azzam.rar@gmail.com>
2023-06-29 13:22:13 +0300

    worked on adding ProjectSettingsVC

---
+++ CAOCAP/ProjectSettingsVC.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectSettingsVC.swift
+//  CAOCAP
+//
+//  Created by Azzam AL-Rashed on 29/06/2023.
+//
+
+import UIKit
+
+class ProjectSettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
