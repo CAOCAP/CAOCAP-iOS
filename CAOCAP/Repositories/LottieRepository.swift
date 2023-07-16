c8a09559937ea22620fdf76f91aea9487c8b37e7
Azzam R Alrashed <azzam.rar@gmail.com>
2023-07-17 00:08:13 +0300

    added Lottie Animations ( confetti & party ) & created LottieRepository

---
+++ CAOCAP/Repositories/LottieRepository.swift
@@ -0,0 +1,21 @@
+//
+//  LottieRepository.swift
+//  CAOCAP
+//
+//  Created by Azzam AL-Rashed on 16/07/2023.
+//
+
+import UIKit
+import Lottie
+
+enum AnimationType: String {
+    case confetti
+    case party
+}
+
+class LottieRepository {
+    static let shared = LottieRepository()
+    
+    
+    
+}
