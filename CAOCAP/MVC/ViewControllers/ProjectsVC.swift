2b0895e8faf3152e8236192eea73c046fbbd0377
Azzam R Alrashed <azzam.rar@gmail.com>
2023-07-24 14:31:18 +0300

    worked on projectCell UI [more work needs to be done here to present the mini mindmap]

--- CAOCAP/MVC/ViewControllers/ProjectsVC.swift
+++ CAOCAP/MVC/ViewControllers/ProjectsVC.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+class ProjectCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var lastUpdated: UILabel!
+}
+
 class ProjectsVC: UIViewController, Storyboarded {
     var coordinator: MainCoordinator?
 
@@ -14,20 +20,43 @@ class ProjectsVC: UIViewController, Storyboarded {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(UITableViewCell.self,forCellReuseIdentifier: "cell")
+        
+        tableView.contentInset.bottom = 40
     }
 }
 
 extension ProjectsVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 10
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "error: failed to load project name"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ProjectCell else { return UITableViewCell() }
+        
+        cell.title.text = "error: failed to load project name"
+        cell.lastUpdated.text = Date.now.formatted()
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("error: failed to load project")
+    }
     
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = UIContextualAction(style: .destructive, title: "delete") { (_, _, _) in
+            print("error: failed to delete project")
+        }
+        
+        let swipeActionConfig = UISwipeActionsConfiguration(actions: [delete])
+        return swipeActionConfig
+    }
 }
