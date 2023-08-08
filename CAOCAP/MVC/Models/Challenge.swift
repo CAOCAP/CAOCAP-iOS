d917121c45cc085bbb826d30523599abeefdca00
Azzam R Alrashed <azzam.rar@gmail.com>
2023-08-08 08:01:53 +0300

    created the Challenge struct Increased app version to 5.0.1

---
+++ CAOCAP/MVC/Models/Challenge.swift
@@ -0,0 +1,14 @@
+//
+//  Challenge.swift
+//  CAOCAP
+//
+//  Created by Azzam AL-Rashed on 08/08/2023.
+//
+
+import Foundation
+
+struct Challenge {
+    let title: String
+    let description: String
+    let regex: String
+}
