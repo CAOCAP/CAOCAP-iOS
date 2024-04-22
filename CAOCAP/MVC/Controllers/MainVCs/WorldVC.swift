e0f4711c6c4fb6d202b1fdd8e2a01c66570edd61
CAOCAP <ficruty@gmail.com>
2024-04-23 02:48:34 +0300

    zoomed in the map view

--- CAOCAP/MVC/Controllers/MainVCs/WorldVC.swift
+++ CAOCAP/MVC/Controllers/MainVCs/WorldVC.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import MapKit
+//import CoreLocation //TODO: add user location
+
 
 class WorldVC: UIViewController, Storyboarded {
     var coordinator: MainCoordinator?
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2D(),
+                                            latitudinalMeters: 1, longitudinalMeters: 1)
         // Do any additional setup after loading the view.
     }
 
