0fe30ad52a0b239a1d2c29597e9861182c031e6a
Azzam R Alrashed <azzam.rar@gmail.com>
2024-08-25 05:41:26 +0300

    fixed the main.js node server issue that was crashing the app if the port was in use

--- CAOCAP/NodeScripts/main.js
+++ CAOCAP/NodeScripts/main.js
@@ -1,15 +1,29 @@
 console.log('Hello from embedded Node.js!');
 
 const http = require('http');
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello from Node.js server on iOS\n');
-}).listen(3000);
 
-/*
- var http = require('http');
- var versions_server = http.createServer((request, response) => {
-     response.end('Versions: ' + JSON.stringify(process.versions));
- });
- versions_server.listen(3000);
- */
+let port = 3000;
+
+// Function to start the server
+const startServer = (port) => {
+    const server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello from Node.js server on iOS\n');
+    });
+
+    server.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+
+    server.on('error', (e) => {
+        if (e.code === 'EADDRINUSE') {
+            console.log(`Port ${port} is in use, trying port ${++port}...`);
+            startServer(port);
+        } else {
+            console.error(`Server error: ${e}`);
+        }
+    });
+};
+
+// Start the server on the initial port
+startServer(port);
