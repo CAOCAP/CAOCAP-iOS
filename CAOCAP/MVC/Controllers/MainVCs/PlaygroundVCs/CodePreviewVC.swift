83b5cb0a432adce8f5d32789c2ed8b5e18a9d20f
Azzam R Alrashed <azzam.rar@gmail.com>
2025-02-18 20:32:22 +0300

    improve the CodePreviewVC

--- CAOCAP/MVC/Controllers/MainVCs/PlaygroundVCs/CodePreviewVC.swift
+++ CAOCAP/MVC/Controllers/MainVCs/PlaygroundVCs/CodePreviewVC.swift
@@ -8,24 +8,37 @@
 import UIKit
 import ReSwift
 import SwiftSoup
+import STTextView
 
 class CodePreviewVC: UIViewController, Storyboarded {
 
     var project: Project?
     @IBOutlet weak var projectTitle: UILabel!
-    @IBOutlet weak var projectCode: UITextView!
+    @IBOutlet weak var projectCodeView: UIView!
+    let textView = STTextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTextView()
+    }
+    
+    func setupTextView() {
+        projectCodeView.addSubview(textView)
+        textView.frame = projectCodeView.bounds
+        textView.font = .monogamma(.regular, size: 15)
+        textView.textColor = .green
+//        textView.isEditable = false TODO: bug in the STTextView package
+        textView.isHorizontallyResizable = true
+        textView.highlightSelectedLine = true
+        textView.showsLineNumbers = true
+        textView.gutterView?.drawSeparator = true
     }
     
     func loadProjectCode() {
         guard let project = project, let document = project.document else { return }
         do {
             projectTitle.text = project.getDocumentTitle()
-            projectCode.text = try document.outerHtml()
+            textView.text = try document.outerHtml()
         } catch Exception.Error(let type, let message) {
             print(type, message)
         } catch {
